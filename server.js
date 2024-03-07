21db754f427fe79665706ccd051150c3c48fc8cf
Mia Chen <mia.chen@nextdrive.io>
2024-03-07 18:03:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const router = require('./router');
-const PORT = process.env.PORT || 3000;
+const router = require('./app/routes/router');
+
+require('dotenv').config();
 
 // Middleware
 app.use(express.json());
 app.use(router);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Backend server is running on port ${PORT}`);
 })
