094deab8ec5913710e496a02cdc1996116837f96
Mia Chen <mia.chen@nextdrive.io>
2024-03-12 12:02:26 +0800

    Update auth.js

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -21,7 +21,41 @@ function generateAccessToken(auth) {
     return jwt.sign({ userId: auth.id }, process.env.JWT_SECRET, { expiresIn: '3d' });
 }
 
+function verifyToken(req, res, next) {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({
+        message: "Your are not authenticated!"
+      });
+    }
+    const token = authHeader.split(" ")[1];
+  
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+      if (err) {
+        return res.status(401).send({
+          message: "Token is not valid!"
+        });
+      }
+      try {
+          const query = 'SELECT * FROM auth WHERE id = $1';
+          const result = await pool.query(query, [decoded.id]);
+          const user = result.rows[0];
+          if (!user) {
+            return res.status(401).send({
+              message: "User not found!"
+            });
+          }
+        req.user = user;
+        next();
+      } catch (error) {
+        console.error('Error retrieving user:', error);
+        res.status(500).json({ error: 'An error occurred while retrieving user information.' });
+      }
+    });
+  };
+
 module.exports = {
     authenticateUser,
-    generateAccessToken
+    generateAccessToken,
+    verifyToken
 };
