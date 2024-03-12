dc5cac791443e5e9157b0a0025200cc14dc791d5
Mia Chen <mia.chen@nextdrive.io>
2024-03-12 18:20:21 +0800

    Update auth.js

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -17,42 +17,53 @@ async function authenticateUser(usermail, password) {
     }
 }
 
-function generateAccessToken(auth) {
-    return jwt.sign({ userId: auth.id }, process.env.JWT_SECRET, { expiresIn: '3d' });
-}
-
 function verifyToken(req, res, next) {
-    let authHeader = req.headers.authorization;
-    if (!authHeader) {
+  let authHeader = req.headers.authorization;
+  if (!authHeader) {
       return res.status(401).send({
-        message: "Your are not authenticated!"
+          message: "Your are not authenticated!"
       });
-    }
-    const token = authHeader.split(" ")[1];
-  
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+  }
+
+  // Extract the token from the authorization header
+  const token = authHeader.split(" ")[1];
+
+  // Verify the token
+  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) {
-        return res.status(401).send({
-          message: "Token is not valid!"
-        });
+          // Handle token verification error
+          console.error('Error verifying token:', err);
+          return res.status(401).send({
+              message: "Token is not valid!"
+          });
       }
+
       try {
+          // Fetch the user from the database using the decoded user id
           const query = 'SELECT * FROM auth WHERE id = $1';
           const result = await pool.query(query, [decoded.id]);
           const user = result.rows[0];
           if (!user) {
-            return res.status(401).send({
-              message: "User not found!"
-            });
+              return res.status(401).send({
+                  message: "User not found!"
+              });
           }
-        req.user = user;
-        next();
+
+          // Assign the user object to the request object
+          req.user = user;
+          // Call the next middleware/route handler
+          next();
       } catch (error) {
-        console.error('Error retrieving user:', error);
-        res.status(500).json({ error: 'An error occurred while retrieving user information.' });
+          console.error('Error retrieving user:', error);
+          res.status(500).json({ error: 'An error occurred while retrieving user information.' });
       }
-    });
-  };
+  });
+};
+
+
+function generateAccessToken(auth) {
+    return jwt.sign({ userId: auth.id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+}
 
 module.exports = {
     authenticateUser,
